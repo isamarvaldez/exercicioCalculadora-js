f7c39bac02a5aad924e450a1a3d60d78e8e1bb1b
isamar valdez <viajesalsur01@gmail.com>
2023-07-27 21:48:45 -0300

    exercicioCalculadora js

--- script.js
+++ script.js
@@ -1,138 +1,173 @@
-//Algoritmo
-//1. Pegar os valores dos imputs
-//2. Fazer o calculo do IMC->  valorIMC
-//3. Gerar a classificacao IMC -> classificacaoIMC
-//4. Organizar os dados do usuarios para salvar na lista e gerar a  data de cadastro
-//5. Inserir o usuario na lista (salvar no localStorage)
-//6. Funcao para carregar os usuarios (localStorage), chamar ao carregar a pagina
-//7. Renderizar o conteudo da tabela com os usuarios cadastrados
-//8. Botao para limpar os registros (localStorage)
+// Algoritmo
+
+// OK   1. Pegar os valores dos inputs
+// OK   2. Fazer o cálculo do IMC -> valorImc
+// OK   3. Gerar a classificação IMC -> classificacaoImc
+// OK   4. Organizar os dados do usuario para salvar na lista e gerar a data de cadastro
+// OK   5. Inserir o usuario na lista (salvar no localStorage)
+// ok   6. Função para carregar os usuarios (localStorage), chamar ao carregar a página
+// ok  7. Renderizar o conteudo da tabela com os usuarios cadastrados
+//      8. Botão para limpar os registros (localStorage)
 
 
 function calcular(event) {
-    // previne recarregar a pagina
+    // Previne o recarregar da página
     event.preventDefault()
-    console.log("foi executada a funcao executar")
-//paso 1
+
+    console.log("Foi executada a função calcular")
+
+    // Passo 1
     let usuario = receberValores()
-    //paso 2
-    let imcCalculado = calcularIMC(usuario.altura, usuario.peso)
-    //paso 3
-    let classificacaoIMC = classificarImc(imcCalculado)
 
-    console.log(classificacaoIMC) 
+    // Passo 2
+    let imcCalculado = calcularImc(usuario.altura, usuario.peso)
 
-//paso 4
-    usuario = organizarDados(usuario, imcCalculado, classificacaoIMC)
+    // Passo 3
+    let classificacaoImc = classificarImc(imcCalculado)
 
-//paso 5
-cadastrarUsuario(usuario)
+    console.log(classificacaoImc)
 
-    function receberValores() {
-        let nomeRecebido = document.getElementById("nome").value.trim()
-        let alturaRecebida = document.getElementById("altura").value
-        let pesoRecebido = document.getElementById("peso").value
+    // Passo 4
+    usuario = organizarDados(usuario, imcCalculado, classificacaoImc)
 
-        let dadosUsuario = {
-            nome: nomeRecebido,
-            altura: alturaRecebida,
-            peso: pesoRecebido
+    // Passo 5
+    cadastrarUsuario(usuario)
+    window.location.reload()
 
-        }
 
-        // console.log(nomeRecebido)
-        // console.log(alturaRcebida)
-        //console.log(pesoRecebido)
-        console.log(dadosUsuario)
 
-        return dadosUsuario
-    }
-    function calcularIMC(altura, peso) {
-        let imc = peso / (altura * altura)
-
-        console.log(imc)
-
-        return imc
-    }
 }
+
+function receberValores() {
+    let nomeRecebido = document.getElementById("nome").value.trim()
+    let alturaRecebida = document.getElementById("altura").value
+    let pesoRecebido = document.getElementById("peso").value
+
+    let dadosUsuario = {
+        nome: nomeRecebido,
+        altura: alturaRecebida,
+        peso: pesoRecebido
+    }
+
+    console.log(dadosUsuario)
+
+    return dadosUsuario
+}
+
+function calcularImc(altura, peso) {
+    let imc = peso / (altura * altura)
+
+    console.log(imc)
+
+    return imc
+}
+
 function classificarImc(imc) {
     /* 
-    Resultado                                         Situacao
-    Entre 18.5                                        Abaixo do peso
-    Entre 18.5 e 24.99                                Peso normal
-    Entre 25 e 29.99                                  Sobrepeso   
-    Acima de 30                                       Obesidade          
+    Resultado               Situação
+
+    Abaixo de 18.5          Abaixo do peso
+    Entre 18.5 e 24.99      Peso normal
+    Entre 25 e 29.99        Sobrepeso
+    Acima de 30             Obesidade    
     */
 
-
-if (imc < 18.5) {
-    return "Abaixo do peso"
-
-} else if (imc >= 18.5 && imc < 25) {
-    return "peso normal"
-
-} else if (imc >= 25 && imc < 30) {
-
-    return "Sobrepeso"
-} else {
-    return "obessidade"
-}
+    if (imc < 18.5) {
+        return "Abaixo do peso"
+    } else if (imc >= 18.5 && imc < 25) {
+        return "Peso normal"
+    } else if (imc >= 25 && imc < 30) {
+        return "Sobrepeso"
+    } else {
+        return "Obesidade"
+    }
 }
 
-function organizarDados(dadosUsuario, valorIMC, classificacaoIMC) {
-    //pegar a dataHoraAtual
-    let dataHoraAtual = new Intl.DateTimeFormat('pt-BR', {timeStyle: 'long', dateStyle: 'short' }).format(Date.now())
+function organizarDados(dadosUsuario, valorImc, classificacaoImc) {
+    // Pegar a dataHoraAtual
+    let dataHoraAtual = new Intl.DateTimeFormat('pt-BR', { timeStyle: 'long', dateStyle: 'short' }).format(Date.now())
 
     console.log(dataHoraAtual);
 
-//organizando objeto para atualizar
+    // Organizando o objeto para salvar
     let dadosUsuarioAtualizado = {
         ...dadosUsuario,
-        imc: valorIMC,
-        situacaoImc: classificacaoIMC,
+        imc: valorImc,
+        situacaoImc: classificacaoImc,
         dataCadastro: dataHoraAtual
     }
 
     return dadosUsuarioAtualizado;
-
-}
-function cadastrarUsuario(dadosUsuario){
-let listaUsuarios = []    // <===LISTA VAZIA OBS: DE ISAMAR
-
-
-if (localStorage.getItem("usuariosCadastrados") != null) {
-    listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
 }
 
-//adiciona o usuaro na lista de usuarios
-listaUsuarios.push(dadosUsuario)
+function cadastrarUsuario(dadosUsuario) {
+    let listaUsuarios = []
 
-
-
-//localStorage.setItem()      //("nomedeusuario", "thiago") EXEMPLO
-
-    //if (localStorage.getItem("usuariosCadastrados") != null){
-   //     listaUsuarios = localStorage.getItem("usuariosCadastrados")
-
-
-   //salva a listaUsuarios no localStorage
-localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
-
+    // Se houver uma lista de usuarios no localStorage, carregar isso para a variavel listaUsuarios
+    if (localStorage.getItem("usuariosCadastrados") != null) {
+        listaUsuarios = JSON.parse( localStorage.getItem("usuariosCadastrados") )
     }
-function  carregarUsuarios(){
-    listaCarregada = []
 
-    if (localStorage.getItem("usuariosCadastrados") != null){
+    // Adiciona o usuario na lista de usuarios
+    listaUsuarios.push(dadosUsuario)
+
+    // Salva a listaUsuarios no localStorage
+    localStorage.setItem("usuariosCadastrados",  JSON.stringify(listaUsuarios) )
+
+}
+
+function carregarUsuarios() {
+    let listaCarregada = []
+
+    if ( localStorage.getItem("usuariosCadastrados") != null ) {
         listaCarregada = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
 
-            }
-        if(listaCarregada.length == 0) {
-            //se nao tiver  nehum usuario cadastrado, mostrar mensagem
-            let tabela = document.getElementById("vorpo-tabela")
+    if(listaCarregada.length == 0) {
+        // Se não tiver nenhum usuario cadastrado, mostrar mensagem
+        let tabela = document.getElementById("corpo-tabela")
 
-            tabela.innerHTML = "nenhum usuario carregado"
-        }
+        tabela.innerHTML =   `<tr class= "linha-mensagem">
+        <td colspan= "6">Nenhum usuario cadastrado 😂 :( </td>
+        </tr`
 
-            console.log(listaCarregada)
- }
- window.addEventListener("DOMcontentLoaded", () => carregarUsuarios())
+    }else { 
+        //montar conteudo da tabela
+        montarTabela(listaCarregada)
+    }
+
+    console.log(listaCarregada)
+}
+
+window.addEventListener("DOMContentLoaded", () => carregarUsuarios() )
+
+//paso 7
+function montarTabela(listaUsuarios) {
+let tabela = document.getElementById("corpo-tabela")
+
+let template = ""
+
+listaUsuarios.forEach(usuario => {
+template += `<tr>
+<td data-cell="nome">${usuario.nome}</td>
+<td data-cell="altura">${usuario.altura}</td>
+<td data-cell="peso">${usuario.peso}</td>
+<td data-cell="valor do IMC">${usuario.imc.toFixed(2)}</td>
+<td data-cell="classificação do IMC">${usuario.situacaoImc}</td>
+<td data-cell="data de cadastro">${usuario.dataCadastro}</td>
+</tr>`         
+                    
+            
+                    
+   // console.log("O usuario é: ", usuario)     
+})
+tabela.innerHTML = template;
+} 
+//remove o item do localStorage
+function deletarRegistros(){
+    localStorage.removeItem("usuariosCadastrados")
+
+    //recareg a pagina
+    window.location.reload()
+
+}
